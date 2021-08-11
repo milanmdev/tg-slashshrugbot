7aed6f867f443d64e017c18d4e0f977003961012
Milan M <milanmdev@gmail.com>
2021-08-11 11:56:28 -0500

    remove mention commands because bots cant access messages ig

--- index.js
+++ index.js
@@ -29,22 +29,6 @@ client.command("source", (ctx) => {
   );
 });
 
-/* Bot Commands */
-client.hears("@" + config.bot_username + " invite", (ctx) => {
-  if (ctx.from.is_bot) return;
-
-  ctx.reply(
-    `👋 Hello there!\n\nYou can invite me to your group chat by adding @slashshrugbot`
-  );
-});
-client.hears("@" + config.bot_username + " source", (ctx) => {
-  if (ctx.from.is_bot) return;
-
-  ctx.reply(
-    `View my source code over at https://github.com/milanmdev/tg-slashshrugbot\n\nNOTE: Support is not provided for selfhosting. You can use this as a guide for making your own bot though :p`
-  );
-});
-
 /* Launch Bot */
 client.launch().then((b) => {
   console.log("Bot launched!");
